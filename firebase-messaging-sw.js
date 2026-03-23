026df1b6bbba5fd014006bfafdfaaf87ff0107f3
Magd El Zalameh <Magd El Zalameh>
2026-03-23 22:33:47 +0200

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+/* eslint-env serviceworker */
+// Import and configure the Firebase SDK
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrmpi4LoNSOdIA1FZiYlbrJoy_KKImiPE",
+  authDomain: "optimist-f04f0.firebaseapp.com",
+  projectId: "optimist-f04f0",
+  storageBucket: "optimist-f04f0.appspot.com",
+  messagingSenderId: "946195402627",
+  appId: "1:946195402627:web:1b85b6f90c107de3a370b6"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/optimist_logo_192.png',
+    data: payload.data
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// Handle notification click
+self.onnotificationclick = (event) => {
+  event.notification.close();
+  const orderId = event.notification.data?.orderId;
+  const urlToOpen = orderId ? `/orders/${orderId}` : '/';
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((windowClients) => {
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        if (client.url === urlToOpen && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(urlToOpen);
+      }
+    })
+  );
+};
